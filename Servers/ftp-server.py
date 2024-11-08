b76966ae1b6650e37925aa2c768efdcbe3217c24
Michaël Roy <microygh@gmail.com>
2024-11-08 12:33:50 +0100

    Add a simple FTP message with server hostname.

--- Servers/ftp-server.py
+++ Servers/ftp-server.py
@@ -13,7 +13,7 @@
 #
 
 # External modules
-import os, tempfile
+import os, platform, tempfile
 from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
@@ -23,22 +23,25 @@ if os.geteuid() != 0 :
 	print( '\n-> Run this application as root (sudo)...')
 	exit()
 
+# Simple FTP message
+FTP_MESSAGE = rf'Bienvenue sur le serveur FTP RT Auxerre de la machine {platform.node()} !'
+
 # Create a temporary directory for the FTP server
 FTP_DIRECTORY = tempfile.TemporaryDirectory()
 
 # Create a temporary text file as an example
 with open( FTP_DIRECTORY.name + '/hello.txt', 'w' ) as f :
-	f.write( 'Bienvenue sur le serveur FTP RT Auxerre !\n' )
+	f.write( FTP_MESSAGE + '\n' )
 	f.close()
 
 # Instantiate a dummy authorizer for managing 'anonymous' users
 authorizer = DummyAuthorizer()
 authorizer.add_anonymous( FTP_DIRECTORY.name )
 
-# Instantiate FTP handler class with TLS support
+# Instantiate FTP handler class
 handler = FTPHandler
 handler.authorizer = authorizer
-handler.banner = 'RT Auxerre FTP Server'
+handler.banner = FTP_MESSAGE
 
 # Start the FTP server
 server = FTPServer( ( '::', 21 ), handler )
