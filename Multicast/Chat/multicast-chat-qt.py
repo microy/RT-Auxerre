4c9b9ebff39e90fb988e05a56cfde609d219e7cf
Michaël Roy <microygh@gmail.com>
2024-11-25 23:46:21 +0100

    Use asyncio.

--- Multicast/Chat/multicast-chat-qt.py
+++ Multicast/Chat/multicast-chat-qt.py
@@ -7,15 +7,36 @@
 #
 
 # External dependencies
-import base64, re, socket, sys
+import asyncio, base64, re, socket, sys
+from threading import Thread
 from PySide6.QtGui import Qt, QKeySequence, QShortcut
-from PySide6.QtNetwork import QHostAddress, QUdpSocket
 from PySide6.QtWidgets import QApplication, QCheckBox, QHBoxLayout, QLabel, QLineEdit, QRadioButton, QTextEdit, QVBoxLayout, QWidget
 
 # Multicast addresses and port
-MULTICAST_ADDRESS4 = QHostAddress( '239.0.0.1' )
-MULTICAST_ADDRESS6 = QHostAddress( 'FF02::239:0:0:1' )
+MULTICAST_ADDRESS4 = '239.0.0.1'
+MULTICAST_ADDRESS6 = 'FF02::239:0:0:1'
 MULTICAST_PORT = 10000
+
+# Chat server protocol
+class ChatProtocol :
+	# Initialisation
+	def __init__( self, message_callback = None ) :
+		# Register the message callback from the application
+		self.message_callback = message_callback
+	# Socket initialisation
+	def connection_made( self, transport ) :
+		# Register the IPv4 multicast group
+		transport.get_extra_info('socket').setsockopt( socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP,
+			socket.inet_aton( MULTICAST_ADDRESS4 ) + socket.inet_aton( '0.0.0.0' ) )
+		# Register the IPv6 multicast group
+		transport.get_extra_info('socket').setsockopt( socket.IPPROTO_IPV6, socket.IPV6_JOIN_GROUP,
+			socket.inet_pton( socket.AF_INET6, MULTICAST_ADDRESS6 ) + socket.inet_pton( socket.AF_INET6, '::' ) )
+	# Message reception
+	def datagram_received( self, message, address ) :
+		# Cleanup the address
+		address = re.sub( r'^::ffff:', '', address[0] )
+		# Send the message to the application
+		self.message_callback( message, address )
 
 # Multicast Chat using Qt
 class QMulticastChat( QWidget ) :
@@ -33,14 +54,12 @@ class QMulticastChat( QWidget ) :
 		# Text edit to show the received messages
 		self.chat = QTextEdit()
 		self.chat.setFocusPolicy( Qt.NoFocus )
-		# Line edit to enter the message to send
-		self.message = QLineEdit()
-		# Signal to know when a new message is entered
-		self.message.returnPressed.connect( self.send_message )
-		# Application layout
+		# Application vertical layout
 		self.layout = QVBoxLayout( self )
+		# Text box for the message received
 		self.layout.addWidget( QLabel( 'Message received :' ) )
 		self.layout.addWidget( self.chat )
+		# Input + options
 		protocols = QHBoxLayout()
 		protocols.addWidget( QLabel( 'Send a message :' ) )
 		protocols.addStretch()
@@ -52,60 +71,46 @@ class QMulticastChat( QWidget ) :
 		self.secret = QCheckBox( 'Secret' )
 		protocols.addWidget( self.secret )
 		self.layout.addLayout( protocols )
+		# Line edit to enter the message to send
+		self.message = QLineEdit()
+		self.message.returnPressed.connect( self.SendMessage )
 		self.layout.addWidget( self.message )
 		self.message.setFocus()
-		# Server connection to receive messages via IPv4
-		self.server4 = QUdpSocket( self )
-		self.server4.bind( QHostAddress.AnyIPv4, MULTICAST_PORT )
-		self.server4.joinMulticastGroup( MULTICAST_ADDRESS4 )
-		self.server4.readyRead.connect( self.receive_message4 )
-		# Server connection to receive messages via IPv6
-		self.server6 = QUdpSocket( self )
-		self.server6.bind( QHostAddress.AnyIPv6, MULTICAST_PORT )
-		self.server6.joinMulticastGroup( MULTICAST_ADDRESS6 )
-		self.server6.readyRead.connect( self.receive_message6 )
 		# Client connection to send messages
-		self.client = QUdpSocket( self )
+		self.client = socket.socket( socket.AF_INET6, socket.SOCK_DGRAM )
+		# Run asyncio loop in another thread
+		loop = asyncio.new_event_loop()
+		asyncio.set_event_loop( loop )
+		Thread( target = loop.run_forever, daemon = True ).start()
+		# Run server in asyncio loop
+		asyncio.run_coroutine_threadsafe( self.RunServer(), loop = loop )
+	# Run server
+	async def RunServer( self ) :
+		await asyncio.get_running_loop().create_datagram_endpoint(
+			lambda : ChatProtocol( self.ReceiveMessage ), local_addr = ( '::', MULTICAST_PORT ) )
 	# Send a message
-	def send_message( self ) :
+	def SendMessage( self ) :
 		# Return if the message is empty
 		if not self.message.text() : return
-		# Encode the message
-		if self.secret.isChecked() : msg = base64.b64encode( self.message.text().encode() )
-		else : msg = self.message.text().encode()
+		# Get the input message
+		message = self.message.text().encode()
+		# Encode the message if secret
+		if self.secret.isChecked() : message = base64.b64encode( message )
 		# Send the message through the IPv4 or IPv6 network
-		if self.button_ipv4.isChecked() : self.client.writeDatagram( msg, MULTICAST_ADDRESS4, MULTICAST_PORT )
-		else : self.client.writeDatagram( msg, MULTICAST_ADDRESS6, MULTICAST_PORT )
+		if self.button_ipv4.isChecked() : self.client.sendto( message, ( f'::ffff:{MULTICAST_ADDRESS4}', MULTICAST_PORT ) )
+		else : self.client.sendto( message, ( MULTICAST_ADDRESS6, MULTICAST_PORT ) )
 		# Clear the text input widget
 		self.message.clear()
-	# Receive the messages
-	def receive_message4( self ) :
-		# Handle all the pending messages
-		while self.server4.hasPendingDatagrams() :
-			# Get the message
-			message, host, _ = self.server4.readDatagram( self.server4.pendingDatagramSize() )
-			# Decode the message
-			if self.secret.isChecked() : msg = base64.b64decode( message.data() ).decode()
-			else : msg = message.data().decode()
-			# Print the message
-			self.chat.append( f'<b>{host.toString()} :</b> {msg}' )
-	# Receive the messages
-	def receive_message6( self ) :
-		# Handle all the pending messages
-		while self.server6.hasPendingDatagrams() :
-			# Get the message
-			message, host, _ = self.server6.readDatagram( self.server6.pendingDatagramSize() )
-			# Decode the message
-			if self.secret.isChecked() : msg = base64.b64decode( message.data() ).decode()
-			else : msg = message.data().decode()
-			# Cleanup the address
-			address = re.sub( r'%.*', '', host.toString() )
-			# Print the message
-			self.chat.append( f'<b>{address} :</b> {msg}' )
+	# Receive a message
+	def ReceiveMessage( self, message, address ) :
+		# Decode the message
+		if self.secret.isChecked() : message = base64.b64decode( message ).decode()
+		else : message = message.decode()
+		# Append the message to the chat history
+		self.chat.append( f'<b>{address} ></b> {message}' )
 
 # Main program
 if __name__ == "__main__" :
-	# Start the Qt application
 	application = QApplication( sys.argv )
 	window = QMulticastChat()
 	window.show()
